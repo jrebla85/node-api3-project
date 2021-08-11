aefad341b91f06791d55a4d321c6ffbf480513db
jeremy black <jrebla85@yahoo.com>
2021-08-10 19:13:39 -0500

    finished writing middleware. Implementing middleware with next push. Installed create react app to attempt stretch later.

--- api/server.js
+++ api/server.js
@@ -5,8 +5,8 @@ const usersRouter = require("./users/users-router");
 const server = express();
 
 server.use(express.json());
-server.use("/api/users", usersRouter);
 server.use(logger);
+server.use("/api/users", usersRouter);
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const Users = require("./users-model");
-const Posts = require("../posts/posts-model");
 
 const { 
-  logger,
   validateUserId,
   validateUser,
   validatePost,
@@ -23,12 +21,12 @@ router.get('/:id', validateUserId, (req, res) => {
   res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -44,7 +42,7 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
